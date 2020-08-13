aa663622b70ea5545752932fd3832fbc544c340b
minhdungdau <buiminhhp92@gmail.com>
2020-08-14 04:24:51 +0700

    bai tap JS

---
+++ javascript/day-3-number-object/number.js
@@ -0,0 +1,81 @@
+//Bài 1: Viết hàm tính thể tích hình cầu, với tham số truyền vào là bán kính của hình cầu.
+
+function volSphere(radius){
+    return Math.pow(radius, 3)* 4/3 * Math.PI
+}
+
+//Bài 2: Viết hàm truyền vào 2 số nguyên, tính tổng tất cả các số nguyên nằm giữa chúng. Ví dụ với 
+//tham số 3 và 8 ta có kết quả là 22 (bằng 4 + 5 + 6 + 7).
+
+function sumIn(int1,int2){
+    if (Number.isInteger(int1) && Number.isInteger(int2) && Math.abs(int1-int2)>1){
+        sum = 0
+        for (i = Math.min(int1, int2) + 1; i < Math.max(int1, int2); i++){
+            sum+=i
+        }
+        return sum
+    } else {return "false"}
+}
+
+//Bài 3: Cho 1 số, kiểm tra xem số đó có phải là số nguyên tố hay không, kết quả trả về true hoặc false. 
+
+function prime(num){
+    if (num > 0 && Number.isInteger(num)){
+        var x = 1
+        for (var i = 2; i < num; i++){
+            if (num % i == 0){
+                x = 0
+            }
+        }
+        if (x == 1){
+            return true
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
+
+//Bài 4: Cho 1 số nguyên dương bất kỳ. Tính tổng tất cả các số nguyên tố mà nhỏ hơn hoặc bằng tham 
+//số truyền vào.
+
+function prime(num){
+    if (num > 0 && Number.isInteger(num)){
+        var x = 1
+        for (var i = 2; i < num; i++){
+            if (num % i == 0){
+                x = 0
+            }
+        }
+        if (x == 1){
+            return true
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
+
+function sumPrime(num){
+    var sum = 0;
+    for (i = 0; i <= num; i++){
+        if (prime(i)){
+            sum+=i
+        }
+    }
+    return sum
+}
+
+//Bài 5: Cho 1 số nguyên dương, viết hàm tính tổng tất cả các ước số của số đó.
+
+function sumUoc(num){
+    sum = 0
+    for (i = 1; i <= num; i++){
+        if (num % i == 0){
+            sum += i
+        }
+    }
+    return sum
+}
---
+++ javascript/day-3-number-object/object.js
@@ -0,0 +1,76 @@
+//Bài 6. Viết hàm để lấy danh sách các key của object
+function key(obj){
+    var arr = []
+    for (x in obj){
+        arr.push(x)
+    }
+    return arr.join()
+}
+
+//Bài 7. Viết hàm để lấy danh sách các value của object
+
+function key(obj){
+    var arr = []
+    for (x in obj){
+        arr.push(obj[x])
+    }
+    return str.join()
+}
+
+//bài 8. Viết hàm kiểm tra key có tồn tại trong Object không
+
+function keyCheck(obj,keyy){
+    var TG = 0
+    for (let x in obj){
+        if (keyy == x){
+            TG = 1
+        }
+    }
+    return TG == 1
+}
+
+//bài 9. Viết hàm kiểm tra Object có độ dài bao nhiêu
+
+function objLength(obj){
+    var dem = 0
+    for (x in obj){
+        dem+=1
+    }
+    return dem
+}
+
+// Bài 10. Cho mảng các user
+// mỗi object có cấu trúc như sau
+// user = {
+//     name : string,
+//     age : number,
+//     isStatus : bool
+// }
+
+// Viết function lấy ra những user có tuổi > 25 và isStatus = true
+
+function userCheck(user){
+    var userChecked=[]
+    for ( i = 0; i < user.length; i++){
+        if (user[i].age > 25 && user[i].isStatus == true){
+            userChecked.push(user[i])
+        }
+    }
+    return userChecked
+}
+
+// Bài 11. Tương tự bài 5
+// Viết function sắp xếp các user theo tuổi tăng dần
+
+function userSort(user){
+    for ( i = 0; i < user.length - 1; i++){
+        for ( j = i + 1; j < user.length; j++){
+            if (user[i].age > user[j].age){
+                var TG = user[i].age
+                user[i].age = user[j].age
+                user[j].age = TG
+            } 
+        }
+    }
+    return user
+}
